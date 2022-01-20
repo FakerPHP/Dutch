7815b1d612ce8b19ae2a1f03be30f81dd4383926
Bram Ceulemans <bramceulemans@me.com>
2022-01-20 21:22:44 +0100

    Add PhoneNumber extension

---
+++ src/NL/PhoneNumber.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Faker\Dutch\NL;
+
+use Faker\Extension\Helper;
+use Faker\Extension\PhoneNumberExtension;
+
+class PhoneNumber implements PhoneNumberExtension
+{
+    /** @var string[] */
+    protected array $formats = [
+        '06 ########',
+        '06-########',
+        '+316-########',
+        '+316 ########',
+        '01# #######',
+        '+311# #######',
+        '02# #######',
+        '+312# #######',
+        '03# #######',
+        '+313# #######',
+        '04# #######',
+        '+314# #######',
+        '05# #######',
+        '+315# #######',
+        '07# #######',
+        '+317# #######',
+        '0800 ######',
+        '+31800 ######',
+        '088 #######',
+        '+3188 #######',
+        '0900 ######',
+        '+31900 ######',
+    ];
+
+    protected string $e164Format = '+31#########';
+
+    public function phoneNumber(): string
+    {
+        return Helper::numerify(Helper::randomElement($this->formats));
+    }
+
+    public function e164PhoneNumber(): string
+    {
+        return Helper::numerify($this->e164Format);
+    }
+}
