e4d857505d4e0072f21e3391a55a6afdf8743572
Bram Ceulemans <bramceulemans@me.com>
2022-01-21 01:09:30 +0100

    Add Person extension

--- src/Factory.php
+++ src/Factory.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Faker\Dutch;
 
+use Faker\Dutch\NL;
+use Faker\Dutch\BE;
 use Faker\Extension;
 use Faker\Extension\ContainerBuilder;
 use Faker\Generator;
@@ -12,33 +14,29 @@ class Factory
 {
     public static function netherlands(): Generator
     {
-        $builder = new ContainerBuilder();
+        $builder = self::getBuilder();
 
-        foreach (ContainerBuilder::defaultExtensions() as $id => $definition) {
-            $builder->add($definition, $id);
-        }
-
-        // Add Dutch specific extensions to replace some default ones
+        // Add specific extensions for The Netherlands to replace some default ones
         $builder->add(NL\Address::class, Extension\AddressExtension::class);
+        $builder->add(NL\Company::class, Extension\CompanyExtension::class);
         $builder->add(NL\Country::class, Extension\CountryExtension::class);
         $builder->add(NL\Person::class, Extension\PersonExtension::class);
+        $builder->add(NL\PhoneNumber::class, Extension\PhoneNumberExtension::class);
 
         $builder->add(NL\Address::class);
+        $builder->add(NL\Company::class);
         $builder->add(NL\Country::class);
         $builder->add(NL\Person::class);
+        $builder->add(NL\PhoneNumber::class);
 
         return new Generator($builder->build());
     }
 
     public static function belgium(): Generator
     {
-        $builder = new ContainerBuilder();
+        $builder = self::getBuilder();
 
-        foreach (ContainerBuilder::defaultExtensions() as $id => $definition) {
-            $builder->add($definition, $id);
-        }
-
-        // Add Dutch specific extensions to replace some default ones
+        // Add specific extensions for Belgium to replace some default ones
         $builder->add(BE\Address::class, Extension\AddressExtension::class);
         $builder->add(BE\Company::class, Extension\CompanyExtension::class);
         $builder->add(BE\Country::class, Extension\CountryExtension::class);
@@ -52,5 +50,16 @@ class Factory
         $builder->add(BE\PhoneNumber::class);
 
         return new Generator($builder->build());
+    }
+
+    private static function getBuilder(): ContainerBuilder
+    {
+        $builder = new ContainerBuilder();
+
+        foreach (ContainerBuilder::defaultExtensions() as $id => $definition) {
+            $builder->add($definition, $id);
+        }
+
+        return $builder;
     }
 }
